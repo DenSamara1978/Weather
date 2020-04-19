c743354f2a1706f6487344757ee15905a9b70dd7
Denis Vlaskin <den_sam@mail.ru>
2020-04-19 23:51:26 +0400

    Intents are ready

---
+++ app/src/main/java/ru/melandra/weather/SettingsActivity.java
@@ -0,0 +1,54 @@
+package ru.melandra.weather;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity implements Constants
+{
+    CheckBox fCheckBox;
+    CheckBox wCheckBox;
+
+    @Override
+    protected void onCreate ( final Bundle savedInstanceState )
+    {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.settings_layout );
+
+        fCheckBox = findViewById ( R.id.fahrenheitScaleCheckBox );
+        wCheckBox = findViewById ( R.id.windShowCheckBox );
+
+        restoreGlobalSettings ();
+        setControls ();
+
+        findViewById ( R.id.saveSettingsButton ).setOnClickListener ( new View.OnClickListener ()
+        {
+            @Override
+            public void onClick ( View view )
+            {
+                GlobalSettings.getInstance ().setFahrenheitScale ( fCheckBox.isChecked () );
+                GlobalSettings.getInstance ().setWindShow ( wCheckBox.isChecked () );
+                saveGlobalSettings ();
+                Toast.makeText ( getApplicationContext (), getString( R.string.settings_saved), Toast.LENGTH_SHORT ).show ();
+            }
+        } );
+    }
+
+    private void setControls () {
+        fCheckBox.setChecked ( GlobalSettings.getInstance ().getFahrenheitScale () );
+        wCheckBox.setChecked ( GlobalSettings.getInstance ().getWindShow () );
+    }
+
+    private void restoreGlobalSettings () {
+        // Первоначальная установка глобальных параметров
+        // В будующем здесь будет установка глобальных параметров из, например, БД
+        // Пока на данный момент, глобальные параметры при запуске приложения будут всегда дефолтными
+    }
+
+    private void saveGlobalSettings () {
+        // Сохранение глобальных настроек в БД
+    }
+}
