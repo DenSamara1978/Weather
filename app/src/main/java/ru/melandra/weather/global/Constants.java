8dd84d5a0eabd0351ff72b3035ae4e3e6ebf3b7f
Denis Vlaskin <den_sam@mail.ru>
2020-05-12 20:57:26 +0400

    ready

--- app/src/main/java/ru/melandra/weather/global/Constants.java
+++ app/src/main/java/ru/melandra/weather/global/Constants.java
@@ -4,6 +4,7 @@ public interface Constants
 {
     String fahrenheitScaleParamName = "GLOBAL_SETTINGS.FAHRENHEIT_SCALE";
     String windShowParamName = "GLOBAL_SETTINGS.WIND_SHOW";
+    String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
 
     String cityNameParam = "CityName";
 }
--- app/src/main/java/ru/melandra/weather/ui/fragments/WeatherFragment.java
+++ app/src/main/java/ru/melandra/weather/ui/fragments/WeatherFragment.java
@@ -3,20 +3,35 @@ package ru.melandra.weather.ui.fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.stream.Collectors;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import com.google.gson.Gson;
+
+import ru.melandra.weather.data.WeatherRequest;
 import ru.melandra.weather.datasources.WeatherDayDataSource;
 import ru.melandra.weather.datasources.WeatherDaySourceBuilder;
 import ru.melandra.weather.global.Constants;
@@ -26,6 +41,7 @@ import ru.melandra.weather.ui.activities.SettingsActivity;
 import ru.melandra.weather.ui.adapters.WeatherDayAdapter;
 
 import static android.app.Activity.RESULT_OK;
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 
 /**
@@ -36,6 +52,8 @@ public class WeatherFragment extends Fragment implements Constants
     private TextView cityNameLabel;
     private ImageButton settingsButton;
     private RecyclerView threeDaysList;
+    private TextView temperatureView;
+    private TextView humidityView;
 
     private final static int REQUEST_CODE = 1;
 
@@ -63,18 +81,17 @@ public class WeatherFragment extends Fragment implements Constants
         Button aboutButton = view.findViewById ( R.id.contextAboutCityButton );
         ImageButton settingsButton = view.findViewById ( R.id.settingsButton );
         threeDaysList = view.findViewById ( R.id.threeDaysList );
+        temperatureView = view.findViewById ( R.id.temperatureView );
+        humidityView = view.findViewById ( R.id.humidityView );
 
-        if ( getArguments () == null )
-            cityNameLabel.setText ( GlobalSettings.getInstance ().getCityName ());
-        else
-            cityNameLabel.setText ( getCurrentCityName ());
+        String cityName = ( getArguments () == null ) ? GlobalSettings.getInstance ().getCityName () : getCurrentCityName ();
+        cityNameLabel.setText ( cityName );
 
         aboutButton.setOnClickListener ( new View.OnClickListener ()
         {
             @Override
             public void onClick ( View view )
             {
-
                 Intent intent = new Intent ( Intent.ACTION_VIEW, Uri.parse ("https://ru.wikipedia.org/wiki/" + cityNameLabel.getText ()));
                 startActivity ( intent );
             }
@@ -92,6 +109,7 @@ public class WeatherFragment extends Fragment implements Constants
 
         initThreeDaysList ();
 
+        requestWeather ( cityName );
         return view;
     }
 
@@ -120,5 +138,53 @@ public class WeatherFragment extends Fragment implements Constants
         DividerItemDecoration itemDecoration = new DividerItemDecoration ( context, LinearLayoutManager.VERTICAL );
         itemDecoration.setDrawable ( context.getDrawable(R.drawable.separator ));
         threeDaysList.addItemDecoration(itemDecoration);
+    }
+
+    private void requestWeather ( String cityName ) {
+        try{
+            String url = WEATHER_URL + "q=" + cityName + "&appid=240af58b6f095eb759a3ecd2d282d448";
+            final URL uri = new URL(url);
+            final Handler handler = new Handler();
+            new Thread(new Runnable() {
+                @RequiresApi (api = Build.VERSION_CODES.N)
+                @Override
+                public void run() {
+                    HttpsURLConnection urlConnection = null;
+                    try{
+                        urlConnection = (HttpsURLConnection) uri.openConnection();
+                        urlConnection.setRequestMethod("GET");
+                        urlConnection.setReadTimeout(10000);
+                        BufferedReader in = new BufferedReader(new InputStreamReader (urlConnection.getInputStream()));
+                        String result = getLines(in);
+                        Gson gson = new Gson();
+                        final WeatherRequest request = gson.fromJson(result, WeatherRequest.class);
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                displayWeather ( request );
+                            }
+                        });
+                    }catch(Exception e){
+                        Toast.makeText ( getContext (), "Connection is failed", Toast.LENGTH_SHORT ).show ();
+                        e.printStackTrace();
+                    }
+                }
+
+                @RequiresApi(api = Build.VERSION_CODES.N)
+                private String getLines(BufferedReader in) {
+                    return in.lines().collect( Collectors.joining("\n"));
+                }
+
+            }).start();
+        }catch(Exception e){
+            Toast.makeText ( getContext (), "Connection is failed", Toast.LENGTH_SHORT).show ();
+        }
+
+    }
+
+    private void displayWeather ( WeatherRequest request ) {
+        cityNameLabel.setText ( request.getName ());
+        temperatureView.setText(String.format("%d°C", (int)(request.getMain().getTemp() - 273.16f )));
+        humidityView.setText(String.format("%d%%", request.getMain().getHumidity()));
     }
 }
