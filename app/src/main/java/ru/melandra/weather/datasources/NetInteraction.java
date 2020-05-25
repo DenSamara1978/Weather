4bedeb81bd984c406cfa6df3f8276ad9276f239b
Denis Vlaskin <den_sam@mail.ru>
2020-05-25 12:49:45 +0400

    Ready

--- app/src/main/java/ru/melandra/weather/datasources/NetInteraction.java
+++ app/src/main/java/ru/melandra/weather/datasources/NetInteraction.java
@@ -22,41 +22,37 @@ import ru.melandra.weather.global.Constants;
 
 public class NetInteraction implements Constants
 {
-    public static void requestWeather ( String cityName, final RequestWeatherReciever receiver ) {
-        try{
-            String url = WEATHER_URL + "q=" + cityName.replace ( " ", "%20" ) + "&appid=" + BuildConfig.API_KEY;
-            final URL uri = new URL(url);
-            new Thread(new Runnable() {
-                @RequiresApi (api = Build.VERSION_CODES.N)
-                @Override
-                public void run() {
+    public static void requestWeather ( final String cityName, final RequestWeatherReciever receiver ) {
+        new Thread(new Runnable() {
+            @RequiresApi (api = Build.VERSION_CODES.N)
+            @Override
+            public void run()
+            {
+                try
+                {
+                    String url = WEATHER_URL + "q=" + cityName.replace ( " ", "%20" ) + "&appid=" + BuildConfig.API_KEY;
+                    URL uri = new URL ( url );
                     HttpsURLConnection urlConnection = null;
-                    try{
-                        urlConnection = (HttpsURLConnection) uri.openConnection();
-                        urlConnection.setRequestMethod("GET");
-                        urlConnection.setReadTimeout(100);
-                        InputStream is = urlConnection.getInputStream ();
-                        InputStreamReader isr = new InputStreamReader ( is );
-                        BufferedReader in = new BufferedReader ( isr);
-                        String result = getLines(in);
- //                       WeatherJsonParser.ParseJson ( result, receiver );
-                        Gson gson = new Gson ();
-                        final WeatherRequest weatherRequest = gson.fromJson ( result, WeatherRequest.class );
-                        Result ( receiver, weatherRequest );
-                    }catch ( final Exception e ) {
-                        Error ( receiver, e );
-                    }
+                    urlConnection = ( HttpsURLConnection ) uri.openConnection ();
+                    urlConnection.setRequestMethod ( "GET" );
+                    urlConnection.setReadTimeout ( 1000 );
+                    InputStream is = urlConnection.getInputStream ();
+                    InputStreamReader isr = new InputStreamReader ( is );
+                    BufferedReader in = new BufferedReader ( isr );
+                    String result = getLines ( in );
+                    WeatherJsonParser.ParseJson ( result, receiver );
                 }
-
-                @RequiresApi(api = Build.VERSION_CODES.N)
-                private String getLines(BufferedReader in) {
-                    return in.lines().collect( Collectors.joining("\n"));
+                catch ( final Exception e )
+                {
+                    Error ( receiver, e );
                 }
+            }
 
-            }).start();
-        }catch(Exception e){
-            Error ( receiver, e );
-        }
+            @RequiresApi ( api = Build.VERSION_CODES.N )
+            private String getLines ( BufferedReader in){
+                return in.lines ().collect ( Collectors.joining ( "\n" ) );
+            }
+        }).start();
     }
 
     private static void Error ( final RequestWeatherReciever receiver, final Exception e )
@@ -71,23 +67,6 @@ public class NetInteraction implements Constants
                 public void run ()
                 {
                     receiver.onError ( e );
-                }
-            } );
-        }
-    }
-
-    private static void Result ( final RequestWeatherReciever receiver, final WeatherRequest weatherRequest )
-    {
-        final Activity activity = receiver.getWeatherReceiverActivity ();
-        if ( activity == null )
-            receiver.onResult ( weatherRequest );
-        else {
-            activity.runOnUiThread ( new Runnable ()
-            {
-                @Override
-                public void run ()
-                {
-                    receiver.onResult ( weatherRequest );
                 }
             } );
         }
